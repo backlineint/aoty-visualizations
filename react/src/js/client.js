20f9a9a7fa8c3895679be633fc4840ca601ccdb1
Brian Perry <brian@brianperryinteractive.com>
2016-11-19 15:11:33 -0600

    Added React and Victory Charts tutorial example

---
+++ react/src/js/client.js
@@ -0,0 +1,96 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { VictoryBar, VictoryChart, VictoryAxis, VictoryTheme, VictoryStack } from 'victory';
+
+const data2012 = [
+  {quarter: 1, earnings: 13000},
+  {quarter: 2, earnings: 16500},
+  {quarter: 3, earnings: 14250},
+  {quarter: 4, earnings: 19000}
+];
+
+const data2013 = [
+  {quarter: 1, earnings: 15000},
+  {quarter: 2, earnings: 12500},
+  {quarter: 3, earnings: 19500},
+  {quarter: 4, earnings: 13000}
+];
+
+const data2014 = [
+  {quarter: 1, earnings: 11500},
+  {quarter: 2, earnings: 13250},
+  {quarter: 3, earnings: 20000},
+  {quarter: 4, earnings: 15500}
+];
+
+const data2015 = [
+  {quarter: 1, earnings: 18000},
+  {quarter: 2, earnings: 13250},
+  {quarter: 3, earnings: 15000},
+  {quarter: 4, earnings: 12000}
+];
+
+class Main extends React.Component {
+  render() {
+    return (
+      <div>
+        <VictoryChart
+          domainPadding={10}
+          theme={VictoryTheme.material}
+        >
+          <VictoryAxis
+            tickValues={["Quarter 1", "Quarter 2", "Quarter 3", "Quarter 4"]}
+          />
+          <VictoryAxis
+            dependentAxis
+            tickFormat={(x) => (`$${x / 1000}k`)}
+          />
+          <VictoryStack
+            colorScale={"warm"}
+          >
+            <VictoryBar
+              data={data2012}
+              x={"quarter"}
+              y={"earnings"}
+            />
+            <VictoryBar
+              data={data2013}
+              x={"quarter"}
+              y={"earnings"}
+            />
+            <VictoryBar
+              data={data2014}
+              x={"quarter"}
+              y={"earnings"}
+            />
+            <VictoryBar
+              data={data2015}
+              x={"quarter"}
+              y={"earnings"}
+            />
+          </VictoryStack>
+        </VictoryChart>
+      </div>
+    );
+  }
+}
+
+/*class Chart2 extends React.Component {
+  render() {
+    return (
+        <div>
+          <h1>Victory Tutorial</h1>
+          <VictoryBar />
+        </div>
+    );
+  }
+}*/
+
+const app = document.getElementById('app');
+ReactDOM.render(<Main />, app);
+
+/*
+* One simple way to handle multiple visualizations would be to add additional classes for the other charts, and then
+* use a select list that on change calls the react render statement to change the graph.
+*/
+//ReactDOM.render(<Chart2 />, app);
