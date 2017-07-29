97683360630c26f9dfe112609717a461ff859256
Brian Perry <contact.brianperry@gmail.com>
2017-07-29 10:52:44 -0500

    Added initial blueprint data table

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import AlbumTable from './components/AlbumTable';
 
 import albums from './albums';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   state = {
@@ -13,13 +11,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Best of Best Of 2016</h2>
-        </div>
-        {/*albumIds.map((key) => {
-          return <p>{albums[key].title}</p>
-        })*/}
+        <h2>Best of Best Of 2016</h2>
         <AlbumTable albums={this.state.albums} />
       </div>
     );
--- react-app/src/components/AlbumTable.js
+++ react-app/src/components/AlbumTable.js
@@ -1,17 +1,58 @@
 import React from 'react';
+import { Cell, Column, Table } from "@blueprintjs/table";
+import '@blueprintjs/core/dist/blueprint.css';
+import '@blueprintjs/table/dist/table.css';
 
 class AlbumTable extends React.Component {
   render() {
     const albums = this.props.albums;
-    const albumIds = Object.keys(albums);
+
+    // Feels like this could be streamlined, but maybe it is a limitation of blueprint...
+    const renderTitleCell = (rowIndex: number) => {
+      return <Cell>{albums[rowIndex].title}</Cell>
+    };
+    const renderGenreCell = (rowIndex: number) => {
+      return <Cell>{decodeURI(albums[rowIndex].field_genre)}</Cell>
+    };
+    const renderAvgCell = (rowIndex: number) => {
+      return <Cell>{albums[rowIndex].field_avg}</Cell>
+    };
+    const renderConsScoreCell = (rowIndex: number) => {
+      return <Cell>{albums[rowIndex].field_cons_score}</Cell>
+    };
+    const renderIdCell = (rowIndex: number) => {
+      return <Cell>{albums[rowIndex].field_id}</Cell>
+    };
+    const renderListsCell = (rowIndex: number) => {
+      return <Cell>{albums[rowIndex].field_lists}</Cell>
+    };
+    const renderTopTenCell = (rowIndex: number) => {
+      return <Cell>{albums[rowIndex].field_top_10s}</Cell>
+    };
+    const renderWtAvgCell = (rowIndex: number) => {
+      return <Cell>{albums[rowIndex].field_wt_avg}</Cell>
+    };
+
     return (
       <div>
-        {albumIds.map((key) => {
-          return <p>{albums[key].title}</p>
-        })}
+        <Table numRows={50}>
+          <Column name="Album" renderCell={renderTitleCell}/>
+          <Column name="Genre" renderCell={renderGenreCell}/>
+          <Column name="Average" renderCell={renderAvgCell}/>
+          <Column name="Consensus Score" renderCell={renderConsScoreCell}/>
+          <Column name="ID" renderCell={renderIdCell}/>
+          <Column name="Lists" renderCell={renderListsCell}/>
+          <Column name="Top Ten" renderCell={renderTopTenCell}/>
+          <Column name="Weighted Average" renderCell={renderWtAvgCell}/>
+        </Table>
       </div>
     )
   }
 }
+
+// TODO - Convert to use npm module prop-types
+AlbumTable.propTypes = {
+  albums: React.PropTypes.array.isRequired
+};
 
 export default AlbumTable;
