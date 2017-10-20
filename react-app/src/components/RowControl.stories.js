7983ce793c8ead927f89709aef5098c3a145a3c7
Brian Perry <contact.brianperry@gmail.com>
2017-10-20 08:52:05 -0500

    Added actions to track handleRowChange in Storybook

--- react-app/src/components/RowControl.stories.js
+++ react-app/src/components/RowControl.stories.js
@@ -9,6 +9,8 @@ import RowControl from './RowControl';
 
 import '@blueprintjs/core/dist/blueprint.css';
 
+// Is there some way to just get import rowControl as defined in app.js? I suppose
+// I could export/import from a config or variables file.
 const rowControl = {
   5: {
     rows: 5,
@@ -34,6 +36,18 @@ const rowControl = {
 
 storiesOf('Row Control', module)
   .addDecorator(withKnobs)
-  .add('Row Control', () => (
-    <RowControl label={text('label', 'Number of Results:')} rowControl={object('rowControl', rowControl)} />
-  ));
+  .add('Row Control', () => {
+    // If we wanted to mock a function passed via props we could do it here
+    // Don't know how to use it to update state/knobs though...
+    /*const handleRowChange = (numRows) => {
+      rowControl[numRows].active = true;
+      console.log(rowControl);
+    };*/
+    return (
+      <RowControl
+        label={text('label', 'Number of Results:')}
+        rowControl={object('rowControl', rowControl)}
+        handleRowChange={ action('handleRowChange') }
+      />
+    );
+  });
