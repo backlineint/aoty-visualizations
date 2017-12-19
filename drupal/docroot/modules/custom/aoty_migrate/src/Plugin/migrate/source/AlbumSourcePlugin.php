cdcccd534809b5647eab08a93ee25794b333800e
Brian Perry <contact.brianperry@gmail.com>
2017-12-18 18:57:06 -0600

    Added missing changes to Album Source Plugin

--- drupal/docroot/modules/custom/aoty_migrate/src/Plugin/migrate/source/AlbumSourcePlugin.php
+++ drupal/docroot/modules/custom/aoty_migrate/src/Plugin/migrate/source/AlbumSourcePlugin.php
@@ -24,7 +24,7 @@ class AlbumSourcePlugin extends Url  {
     // TODO - Store Secret in Config
     $session = new SpotifyWebAPI\Session(
       '9fe9bff24f2a46cd8c76b7378ea1160a',
-      'SECRET GOES HERE'
+      'secret'
     );
 
     $session->requestCredentialsToken();
@@ -45,12 +45,19 @@ class AlbumSourcePlugin extends Url  {
 
       //print "Title: " . $album_title . PHP_EOL;
 
-      // Could we add artist and album to query?
-      // Or can we just hardcode some corrections to the searches here?
       $search = $api->search($album_title, 'album');
-      $spotify_album_id = $search->albums->items[0]->id;
+
+      // Can't find a way to query based on artist and album, so instead we'll
+      // check the results to see if we have the correct album.
+      foreach ($search->albums->items as $key => $album) {
+        if ((strtolower($album->artists[0]->name) == strtolower($artist)) && (strtolower($album->name) == strtolower($album_title))) {
+          break;
+        }
+      }
+
+      $spotify_album_id = $search->albums->items[$key]->id;
       // images[0] would be 640 rather than 300.
-      $cover_image = $search->albums->items[0]->images[2]->url;
+      $cover_image = $search->albums->items[$key]->images[2]->url;
 
       $row->setSourceProperty('artist', $artist);
       $row->setSourceProperty('album_title', $album_title);
@@ -60,6 +67,35 @@ class AlbumSourcePlugin extends Url  {
       // Don't make the spotify API too mad.
       sleep(1);
     }
+
+    $list_rankings = array();
+    // Do we need to default the value to 0 if no value exists?
+    $list_rankings[] = "rtrade:" . $row->getSourceProperty('rtrade');
+    $list_rankings[] = "rstone:" . $row->getSourceProperty('rstone');
+    $list_rankings[] = "q:" . $row->getSourceProperty('q');
+    $list_rankings[] = "paste:" . $row->getSourceProperty('paste');
+    $list_rankings[] = "nme:" . $row->getSourceProperty('nme');
+    $list_rankings[] = "mojo:" . $row->getSourceProperty('mojo');
+    $list_rankings[] = "drwnd:" . $row->getSourceProperty('drwnd');
+    $list_rankings[] = "cos:" . $row->getSourceProperty('cos');
+    $list_rankings[] = "uncut:" . $row->getSourceProperty('uncut');
+    $list_rankings[] = "skinny:" . $row->getSourceProperty('skinny');
+    $list_rankings[] = "cmplx:" . $row->getSourceProperty('cmplx');
+    $list_rankings[] = "crack:" . $row->getSourceProperty('crack');
+    $list_rankings[] = "gvsb:" . $row->getSourceProperty('gvsb');
+    $list_rankings[] = "noisey:" . $row->getSourceProperty('noisey');
+    $list_rankings[] = "quietus:" . $row->getSourceProperty('quietus');
+    $list_rankings[] = "strgum:" . $row->getSourceProperty('strgum');
+    $list_rankings[] = "uproxx:" . $row->getSourceProperty('uproxx');
+    $list_rankings[] = "vnlfct:" . $row->getSourceProperty('vnlfct');
+    $list_rankings[] = "wire:" . $row->getSourceProperty('wire');
+    $list_rankings[] = "popmat:" . $row->getSourceProperty('popmat');
+    $list_rankings[] = "pfork:" . $row->getSourceProperty('pfork');
+    $list_rankings[] = "npr:" . $row->getSourceProperty('npr');
+
+    $row->setSourceProperty('list_rankings', $list_rankings);
+
+    // Pick up - get all lists in and commit.
 
     return parent::prepareRow($row);
   }
